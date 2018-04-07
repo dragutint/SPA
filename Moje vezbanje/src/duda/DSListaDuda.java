05336dcf0ffb217c5a18920d4f161eb39fb854d8
duda973 <duda.todorovic.97@gmail.com>
2018-04-07 15:33:29 +0200

    DSListaDuda

--- Moje vezbanje/src/duda/DSListaDuda.java
+++ Moje vezbanje/src/duda/DSListaDuda.java
@@ -1,5 +1,23 @@
 package duda;
 
-public class DSListaDuda {
+import labis.cvorovi.CvorJSListe;
+import labis.liste.ADSLista;
 
+public class DSListaDuda extends ADSLista {
+
+	// prikaz liste
+	public void ispisiListu(CvorJSListe prvi) {
+		if (prvi == null) {
+			return;
+		}
+
+		CvorJSListe pom = prvi;
+		while (pom != null) {
+			System.out.print(pom.podatak + ", ");
+			pom = pom.sledeci;
+		}
+	}
+
+	
+	
 }
