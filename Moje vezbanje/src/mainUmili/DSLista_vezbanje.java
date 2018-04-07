c7313934bafde7d815d2d4d73d68df5f5266b598
umicevicivan <umicevic.ivan@gmail.com>
2018-04-07 15:27:25 +0200

    Jel radi ovaj kurac??!?!

---
+++ Moje vezbanje/src/mainUmili/DSLista_vezbanje.java
@@ -0,0 +1,136 @@
+package mainUmili;
+
+import labis.cvorovi.CvorDSListe;
+import labis.exception.LabisException;
+import labis.liste.ADSLista;
+
+public class DSLista_vezbanje extends ADSLista {
+
+	// oleee radi
+	public CvorDSListe zameniPrvaDva(CvorDSListe prvi) throws LabisException {
+
+		if (prvi == null) {
+			throw new LabisException("Lista ne postoji");
+		}
+		if (prvi.sledeci == null) {
+			throw new LabisException("U listi je samo jedan element");
+		}
+
+		prvi = prvi.sledeci;
+
+		prvi.prethodni.sledeci = prvi.sledeci;
+
+		prvi.sledeci = prvi.prethodni;
+
+		prvi.prethodni = prvi.sledeci.prethodni;
+
+		prvi.sledeci.prethodni = prvi;
+
+		prvi.sledeci.sledeci.prethodni = prvi.sledeci;
+
+		return prvi;
+	}
+
+	public int vratiProizvod(CvorDSListe prvi) throws LabisException {
+
+		if (prvi == null) {
+			throw new LabisException("Lista ne postoji");
+		}
+		if (prvi.sledeci == null) {
+			throw new LabisException("Lista ima samo jedan element");
+		}
+		if (prvi.sledeci.sledeci == null) {
+			throw new LabisException("Lista ima dva elementa");
+		}
+
+		CvorDSListe pom = prvi.sledeci;
+		int b = 0;
+		int proizvod = 1;
+		while (pom.sledeci != null) {
+
+			if (pom.podatak % 2 == 1 && pom.sledeci.podatak % 2 == 0 && pom.prethodni.podatak % 2 == 0) {
+				proizvod = proizvod * pom.podatak;
+				b++;
+			}
+
+			pom = pom.sledeci;
+		}
+
+		if (b == 0) {
+			throw new LabisException("Lista je super ali nema takav element");
+		}
+
+		return proizvod;
+
+	}
+
+	public CvorDSListe kloniraj(CvorDSListe prvi) throws LabisException{
+		
+		if(prvi == null) {
+			throw new LabisException("Lista ne postoji");
+		}
+		
+		ADSLista lista  = new ADSLista() {
+		};
+		
+		//ubaujem prvi element koji znam da postoji 100%
+		CvorDSListe novi = new CvorDSListe(prvi.podatak, null, null);
+		lista.prvi = novi;
+		
+		CvorDSListe pom = prvi.sledeci;
+		CvorDSListe pom2 = lista.prvi;
+		while(pom != null) {
+			CvorDSListe novi1 = new CvorDSListe(pom.podatak, pom2, null);
+			pom2.sledeci = novi1;
+			pom2 = novi1;
+			pom = pom.sledeci;
+		}
+		
+		return lista.prvi;
+			
+	}
+	
+	public CvorDSListe invertujBezNovih(CvorDSListe prvi) throws LabisException{
+		
+		if(prvi == null || prvi.sledeci == null) {
+			return prvi;
+		}
+		
+		CvorDSListe pom = prvi;
+		CvorDSListe pom2 = prvi;
+		
+		while(pom2.sledeci.sledeci != null) {
+			pom = pom2.sledeci;
+			pom2.sledeci = pom.sledeci;
+			pom.sledeci.prethodni = pom2; // ovde ce da vrati null poin ako u uslovu ostane samo po2.sledeci != null, zato na kraju moram da dodam poslednji
+			pom.sledeci = prvi;
+			pom.prethodni = prvi.prethodni;
+			prvi.prethodni = pom;
+			
+			prvi = pom;
+		}
+		//poslednji ubacujem na pocetak
+		pom = pom2.sledeci;
+		pom2.sledeci = null;
+		pom.sledeci = prvi;
+		pom.prethodni = null;
+		prvi.prethodni = pom;
+		prvi = pom;
+		
+		
+		return prvi;
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Moje vezbanje/src/mainUmili/Nizovi_sam.java
@@ -0,0 +1,112 @@
+package mainUmili;
+
+import labis.exception.LabisException;
+import labis.niz.ANiz;
+
+public class Nizovi_sam extends ANiz {
+	
+	//ispisuje elemente koji se NE pojavljuju vise od jednom
+	public void zzv10(int niz[]) throws LabisException {
+
+		if (niz == null || niz.length == 0) {
+			throw new LabisException("Niz ili ne postoji ili je prazan");
+		}
+		int brojac = 0;
+		for (int i = 0; i < niz.length; i++) {
+			for (int j = 0; j < niz.length; j++) {
+				if (niz[i] == niz[j]) {
+					brojac++;
+				}
+			}
+			if (brojac == 1) {
+				System.out.println(niz[i]);
+			}
+			brojac = 0;
+		}
+
+	}
+	
+	//vraca dvocifreni iz niza jednocifrenih, dvocifrenih i trocifrenih
+	public int zzv18(int niz[]) throws LabisException {
+		
+		if (niz == null || niz.length == 0) {
+			throw new LabisException("Niz ili ne postoji ili je prazan");
+		}
+		int brojac = 0;
+		int pom = 0;
+		
+		for(int i = 0; i<niz.length; i++) {
+			pom = niz[i];
+			while(pom >= 1) {
+				pom = pom / 10;
+				brojac++;
+			}
+			
+			if(brojac == 2) {
+				System.out.println(niz[i]);
+				return niz[i];
+			}
+			brojac = 0;
+		}
+		
+		throw new LabisException("Ne postoji dvocifreni");
+		
+	}
+	
+	//Od dva napravi jedan koji je u rastucem redosledu ( nije uradjen sort)
+	public int[] zzv22(int a[], int b[]) {
+		
+		if (a == null || a.length == 0) {
+			return b;
+		}
+		if (b == null || b.length == 0) {
+			return a;
+		}
+
+		int[] novi = new int[a.length + b.length];
+		int brojac = 0;
+		for (int i = 0; i < a.length; i++) {
+			novi[i] = a[i];
+			brojac++;
+		}
+		for (int i = 0; i < b.length; i++) {
+			novi[brojac] = b[i];
+			brojac++;
+		}
+
+		return novi;
+	}
+	
+	public int treciNajveci(int a[]) throws LabisException {
+		
+		if (a == null) {
+			throw new LabisException("Niz ne postoji");
+		}
+		if(a.length < 3) {
+			throw new LabisException("U nizu ima manje od 3 elementa");
+		}
+		
+		int m1 = Integer.MIN_VALUE;
+		int m2 = Integer.MIN_VALUE;
+		int m3 = Integer.MIN_VALUE;
+
+		
+		for (int i = 0; i < a.length; i++) {
+			if(a[i] > m1) {
+				m3 = m2;
+				m2 = m1;
+				m1 = a[i];
+			}
+			else if(a[i] > m2) {
+				m3 = m2;
+				m2 = a[i];
+			}
+			else if(a[i] > m3) {
+				m3 = a[i];
+			}
+		}
+		return m3;
+	}
+
+}
+
